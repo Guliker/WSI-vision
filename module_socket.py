48cc62dff5d4c61d53e49132364c919af9cca035
Guliker <81476720+Guliker@users.noreply.github.com>
2021-11-11 14:52:18 +0100

    made vision server

--- module_socket.py
+++ module_socket.py
@@ -7,9 +7,10 @@ Asks for data and receives the whole product
 import socket
 from ast import literal_eval
 # IJsel laptop
-HOST = '192.168.14.219'
+#HOST = '192.168.14.219'
 # Christiaan laptop
 #HOST = '192.168.14.174'
+HOST = ''
 PORT = 30000
 
 command_message = b"asking_for_file"
@@ -20,17 +21,19 @@ def ask_for_data():
     :brief      Start connection with the socket and asking for an available block on the socket
     :return     Array of items that are the product
     """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(5)
+    print("")
+    print("-- Waiting for data ..."),
+    c, addr = s.accept()
+    print(" Message received --") 
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((HOST, PORT))
-        #print("-- sending vision --")
-        s.sendall(command_message)
-        data = s.recv(1024)
-
-        #print("-- data --")
-        #print('Received', repr(data))
-        return literal_eval(data)
+        recv_msg = c.recv(1024)
+        print("Message: ", recv_msg)
+        return literal_eval(recv_msg)
     except socket.error as socketerror:
-        print("---socket busy---")
+        print("socket error")
     
     return "[]"
