88a34cf0c64e4daad9003ab6d21837ce3efa5c2d
Guliker <81476720+Guliker@users.noreply.github.com>
2021-11-25 11:43:23 +0100

    Doxygen and UML update

--- _doxygen/html/search/all_3.js
+++ _doxygen/html/search/all_3.js
@@ -9,9 +9,10 @@ var searchData=
   ['color_5fspace_6',['color_space',['../namespacemain.html#ad85f2075474fba5d346d3e9f413e2be6',1,'main']]],
   ['commandfeature_7',['CommandFeature',['../classgxipy_1_1gxiapi_1_1_command_feature.html',1,'gxipy::gxiapi']]],
   ['config_8',['config',['../namespacemodule__camera.html#aab8087e81af463ac3d6e431ee75ce0d6',1,'module_camera']]],
-  ['convert_9',['convert',['../classgxipy_1_1gxiapi_1_1_raw_image.html#a6e6524c568ee255ba3b9911df5259478',1,'gxipy::gxiapi::RawImage']]],
-  ['create_5fbit_5fmask_5ffor_5fcolor_10',['create_bit_mask_for_color',['../namespacemodule__functions.html#a5944e9f9cb7cd5fe753df84013c2adfe',1,'module_functions']]],
-  ['create_5fblock_5fpos_5farray_11',['create_block_pos_array',['../namespacemodule__functions.html#a68b59d76c84726096c32dea4b8b298f5',1,'module_functions']]],
-  ['create_5fcontour_12',['create_contour',['../namespacemodule__functions.html#aa96152cc4a4572f7b29a330a49cb2d76',1,'module_functions']]],
-  ['create_5fworkspace_13',['create_workspace',['../namespacemodule__functions.html#adcd0636858e1ece6bff7046eeb0f0fe7',1,'module_functions']]]
+  ['contour_5fto_5fpoints_9',['contour_to_points',['../namespacemodule__workspace.html#af32f4cde408ce492f99734106ceb4655',1,'module_workspace']]],
+  ['convert_10',['convert',['../classgxipy_1_1gxiapi_1_1_raw_image.html#a6e6524c568ee255ba3b9911df5259478',1,'gxipy::gxiapi::RawImage']]],
+  ['create_5fbit_5fmask_5ffor_5fcolor_11',['create_bit_mask_for_color',['../namespacemodule__functions.html#a5944e9f9cb7cd5fe753df84013c2adfe',1,'module_functions']]],
+  ['create_5fblock_5fpos_5farray_12',['create_block_pos_array',['../namespacemodule__functions.html#a68b59d76c84726096c32dea4b8b298f5',1,'module_functions']]],
+  ['create_5fcontour_13',['create_contour',['../namespacemodule__functions.html#aa96152cc4a4572f7b29a330a49cb2d76',1,'module_functions']]],
+  ['create_5fworkspace_14',['create_workspace',['../namespacemodule__workspace.html#a4ec08dad96f74dd9e2a37fa162925570',1,'module_workspace']]]
 ];
--- _doxygen/html/search/all_6.js
+++ _doxygen/html/search/all_6.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['feature_0',['Feature',['../classgxipy_1_1gxiapi_1_1_feature.html',1,'gxipy::gxiapi']]],
   ['featuretypeerror_1',['FeatureTypeError',['../classgxipy_1_1gxiapi_1_1_feature_type_error.html',1,'gxipy::gxiapi']]],
-  ['find_5fangle_5fbetween_2',['find_angle_between',['../namespacemodule__functions.html#a7b1acafebdc5cc0ef48f7202a12ad056',1,'module_functions']]],
-  ['find_5fcontour_5fangle_3',['find_contour_angle',['../namespacemodule__functions.html#af512726cd648e41774ce21b1d18e0a47',1,'module_functions']]],
+  ['find_5fangle_5fbetween_2',['find_angle_between',['../namespacemodule__workspace.html#ac943044a5a7c1bc6513fd81773c147de',1,'module_workspace']]],
+  ['find_5fcontour_5fangle_3',['find_contour_angle',['../namespacemodule__workspace.html#adb1e680e28039cab8533095d299a5798',1,'module_workspace']]],
   ['find_5fmin_5fmax_5farray_4',['find_min_max_array',['../namespacemodule__functions.html#a853d161a502bcfd87493ab3aa6b0d78b',1,'module_functions']]],
   ['find_5fmin_5fmax_5fcolor_5',['find_min_max_color',['../namespacemodule__functions.html#a2161d037973c5445e421c31e0f50ceb6',1,'module_functions']]],
   ['floatfeature_6',['FloatFeature',['../classgxipy_1_1gxiapi_1_1_float_feature.html',1,'gxipy::gxiapi']]],
