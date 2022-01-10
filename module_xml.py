2ee6c91623b74cc2601ddd0a1d9b0e15ef214f14
Guliker <81476720+Guliker@users.noreply.github.com>
2022-01-10 14:29:05 +0100

    comments

--- module_xml.py
+++ module_xml.py
@@ -13,11 +13,12 @@ def xml_generate(block_and_pos, save_path_file = "C://ViewerOrder/Order.xml"):
     :param      block_and_pos:      Array of the block lane and position
     :param      save_path_file:     The path and file where the made xml file will be saved
     """
+    # set header of xml file
     root = minidom.Document()
-
     xml = root.createElement('LEGOORDER') 
     root.appendChild(xml)
 
+    # add each block to the xml file
     for i, info in enumerate(block_and_pos):
         pos = cfg.map_pos[info[1]]
         size = cfg.map_block[info[0]][1]
@@ -29,10 +30,12 @@ def xml_generate(block_and_pos, save_path_file = "C://ViewerOrder/Order.xml"):
         productChild.setAttribute('Color', str(color))
         xml.appendChild(productChild)
 
+    # convert to xml string to save
     xml_str = root.toprettyxml(indent ="\t") 
 
+    # save xml file to 'save_path_file'
     with open(save_path_file, "w") as f:
         f.write(xml_str) 
 
-#cleares file
+#cleares file when module is loaded
 xml_generate(())
